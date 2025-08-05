89d1abe00c1f360ec608619923f2a337eab0d768
Chandrashekhar Satav <cpsatav@gmail.com>
2025-08-05 19:05:37 +0530

    function to calculate credit score added with logistic function

--- app/prediction_helper.py
+++ app/prediction_helper.py
@@ -15,7 +15,7 @@ features = model_data['features']
 cols_to_scale = model_data['cols_to_scale']
 
 
-def predict(age, income, loan_amount, loan_tenure_months, avg_dpd_per_delinquency,
+def prepare_df(age, income, loan_amount, loan_tenure_months, avg_dpd_per_delinquency,
             delinquency_ratio, credit_utilization_ratio, num_open_accounts,
             residence_type, loan_purpose, loan_type):
     # Prepare input data
@@ -47,4 +47,53 @@ def predict(age, income, loan_amount, loan_tenure_months, avg_dpd_per_delinquenc
         'enquiry_count': 1  # Dummy value
      }
 
-        return probability, credit_score, rating
+        # Ensure all columns for features and cols_to_scale are present
+        df = pd.DataFrame([input_data])
+
+        # Ensure only required columns for scaling are scaled
+        df[cols_to_scale] = scaler.transform(df[cols_to_scale])
+
+        # Ensure the DataFrame contains only the features expected by the model
+        df = df[features]
+
+        return df
+
+def predict(age, income, loan_amount, loan_tenure_months, avg_dpd_per_delinquency,
+            delinquency_ratio, credit_utilization_ratio, num_open_accounts,
+            residence_type, loan_purpose, loan_type):
+    # Prepare input data
+    input_df = prepare_input(age, income, loan_amount, loan_tenure_months, avg_dpd_per_delinquency,
+                             delinquency_ratio, credit_utilization_ratio, num_open_accounts, residence_type,
+                             loan_purpose, loan_type)
+
+    probability, credit_score, rating = calculate_credit_score(input_df)
+
+    return probability, credit_score, rating
+        
+def calculate_credit_score(input_df, base_score=300, scale_length=600):
+    x = np.dot(input_df.values, model.coef_.T) + model.intercept_
+
+    # Apply the logistic function to calculate the probability
+    default_probability = 1 / (1 + np.exp(-x))
+
+    non_default_probability = 1 - default_probability
+
+    # Convert the probability to a credit score, scaled to fit within 300 to 900
+    credit_score = base_score + non_default_probability.flatten() * scale_length
+
+    # Determine the rating category based on the credit score
+    def get_rating(score):
+        if 300 <= score < 500:
+            return 'Poor'
+        elif 500 <= score < 650:
+            return 'Average'
+        elif 650 <= score < 750:
+            return 'Good'
+        elif 750 <= score <= 900:
+            return 'Excellent'
+        else:
+            return 'Undefined'  # in case of any unexpected score
+
+    rating = get_rating(credit_score[0])
+
+    return default_probability.flatten()[0], int(credit_score[0]), rating
